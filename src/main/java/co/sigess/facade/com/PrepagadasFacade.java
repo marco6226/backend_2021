80bd3554afc80f50c03badf3b2f7bba48bb4ccab
Marco Acero <marco_haz24@hotmail.com>
2021-07-29 15:59:45 -0500

    entities prepagadas

---
+++ src/main/java/co/sigess/facade/com/PrepagadasFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.sigess.facade.com;
+
+import co.sigess.entities.com.Prepagadas;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author fmoreno
+ */
+@Stateless
+public class PrepagadasFacade extends AbstractFacade<Prepagadas> {
+
+    @PersistenceContext(unitName = "SIGESS_PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PrepagadasFacade() {
+        super(Prepagadas.class);
+    }
+    
+}
