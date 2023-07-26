44385ec0a1f07d57d1d7b62609949186566a26bd
Juanse-92 <juanbernal@lerprevencion.com>
2023-07-26 16:22:24 -0500

    email responasable telefonica desviacion

--- src/main/java/co/sigess/entities/sec/Desviacion.java
+++ src/main/java/co/sigess/entities/sec/Desviacion.java
@@ -87,6 +87,18 @@ public class Desviacion implements Serializable {
      
     @Column(name = "causo_muerte")
     private boolean causoMuerte;
+    
+    @Column(name = "email")
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+        
 
     public String getCriticidad() {
         return criticidad;
